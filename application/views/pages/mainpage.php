db49b6884499338bf3d59f9785480641c0bb1b75
Suphanan Yolao <suphanan.yo@ntt.co.th>
2018-01-22 14:33:37 +0700

    design view

--- application/views/pages/mainpage.php
+++ application/views/pages/mainpage.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
+<style>
+    .dataTables_filter {
+        float: right;
+    }
+</style>
 <body>
     <!-- partial -->
     <div class="col-lg-12">
@@ -10,17 +15,18 @@
             <div class="card-body">
                 <h5 class="card-title mb-4">Hardware MA Lists</h5>
                 <div class="table-responsive">
-                    <table class="table center-aligned-table">
+                    <!-- <table class="table center-aligned-table"> -->
+                    <table id="example" class="table table-bordered" cellspacing="0" width="100%">
                     <thead>
                         <tr class="text-primary">
-                        <th>No</th>
-                        <th>Model</th>
-                        <th>Serial Number</th>
-                        <th>Asset No.</th>
-                        <th>HW-Code</th>
-                        <th>Warranty Expire</th>
-                        <th>Check by</th>
-                        <th></th>
+                            <th class="text-center">No</th>
+                            <th class="text-center">Model</th>
+                            <th class="text-center">Serial Number</th>
+                            <th class="text-center">Asset No.</th>
+                            <th class="text-center">HW-Code</th>
+                            <th class="text-center">Warranty Expire</th>
+                            <th class="text-center">Check by</th>
+                            <th class="text-center">Option</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -43,7 +49,7 @@
                             <td>FHK093211RB</td>
                             <td>6652</td>
                             <td>-</td>
-                            <td class="text-center"><label class="badge badge-success">9/30/2013</label></td>
+                            <td class="text-center"><label class="badge badge-success">10/26/2013</label></td>
                             <td>Pong Ju</td>
                             <td class="text-center">
                                 <button type="button" class="btn btn-outline-primary btn-sm view-item" data-id="2">View</button>
@@ -61,8 +67,17 @@
 </body>
 
 </html>
+<script src="<?=base_url('assets/bower_components/datatables.net/js/jquery.dataTables.min.js');?>"></script>
 <script>
     $(document).ready(function(){
+        $('#example').DataTable({
+          "paging": false,
+          dom:"<'row'<'col-sm-6 create-request export-request'l><'col-sm-6'f>>"
+        });
+        
+        var button_create = '<a href="#" class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> Create request</a>'; 
+        $('.create-request').append(button_create);
+
         $(".view-item").click(function(){
         console.log($(this).data('id'));
         var item_id = $(this).data('id');
